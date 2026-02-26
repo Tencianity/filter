412b765d480e44fd3ed585058527ad3ec4c7e1e3
Tencianity <arendechabert@gmail.com>
2026-02-26 15:16:07 -0500

    Organized some files into appropriate folders

--- filter.c
+++ filter.c
@@ -33,8 +33,12 @@ int main(int argc, char *argv[])
     }
 
     // Remember filenames
-    char *infile = argv[optind];
-    char *outfile = argv[optind + 1];
+    char infile[100];
+    char outfile[100];
+    strcpy(infile, "images/");
+    strcpy(outfile, "output/");
+    strcat(infile, argv[optind]);
+    strcat(outfile, argv[optind + 1]);
 
     // Open input file
     FILE *inptr = fopen(infile, "r");
